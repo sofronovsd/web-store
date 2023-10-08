4e50a46735413cbee40ee0edf9f89e508dc310ac
Semyon Sofronov <semyon.sofronov@coolblue.nl>
2023-10-08 16:36:49 +0200

    add router and header component for the main page

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,45 @@
+import {
+  Box,
+  ButtonGroup,
+  Flex,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react';
+import { CloseIcon, SearchIcon } from '@chakra-ui/icons';
+import { CartIcon } from '../../icons';
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      justifyContent="space-evenly"
+      alignItems="center"
+      gap="2"
+      p="2"
+    >
+      <Box>
+        <Heading color="gray">WebStore</Heading>
+      </Box>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none" children={<SearchIcon />} />
+        <Input type="search" placeholder="Search..." />
+        <InputRightElement
+          children={
+            <IconButton
+              variant="ghost"
+              icon={<CloseIcon />}
+              aria-label="Reset the search input value"
+            />
+          }
+        />
+      </InputGroup>
+      <ButtonGroup>
+        <IconButton aria-label="Show the cart" icon={<CartIcon />} />
+      </ButtonGroup>
+    </Flex>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import { router } from './router.tsx';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
